8fe9f9e623294ef0e955324ff04ade2012ecf794
euuns <dmsu2@naver.com>
2025-08-23 16:13:59 +0900

    Core: 프로젝트 생성

---
+++ src/main/java/com/example/readus/ReadUsApplication.java
@@ -0,0 +1,13 @@
+package com.example.readus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/readus/ReadUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
